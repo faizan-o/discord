9c858075152a9f98a79b64b6d8edfc4bd0ddd7e7
faizan-o <roocking.prince@gmail.com>
2024-08-12 19:24:49 +0500

    Fixed Bugs

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,12 +9,6 @@ const isPublicRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  if (
-    req.headers.get("accept") &&
-    req.headers.get("accept")?.includes("text/html")
-  ) {
-    return;
-  }
   if (!isPublicRoute(req)) auth().protect();
 });
 
